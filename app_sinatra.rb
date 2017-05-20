ad233438600747f130c36978daba6b679ced0bb2
anncaly <anncaly@gmail.com>
2017-05-20 13:35:32 +0300

    sinatra practice 2, create all main project files and db

--- app_sinatra.rb
+++ app_sinatra.rb
@@ -4,32 +4,66 @@ require 'sinatra/namespace'
 
 require 'sinatra/base'
 
-module AppModule
-  class App < Sinatra::Base
-    register Sinatra::Namespace
+require 'sequel'
+require 'sequel/extensions/seed'
+require 'pg'
+require 'json'
+require 'multi_json'
 
-    get '/' do
-      'Hello My Sinatra - Easy and Wide World!'
-    end
+# module AppModule
+#   class App < Sinatra::Base
+#     register Sinatra::Namespace
 
-    get '/hello/name' do
-      "Sinatra приветствует тебя, #{params[:name]}!"
-    end
 
-    namespace '/api/v1' do
-      get '/*' do
-        "I don't know what is the #{params[:splat]}. It's what you typed."
-      end
-    end
+DB = Sequel.connect(
+    adapter: :postgres,
+    database: 'sinatra_seq_dev',
+    host: 'localhost',
+    password: 'password',
+    user: 'sinatra_admin',
+    max_connections: 10,
+# logger: Logger.new('log/db.log')
+)
 
-    get '/v' do
-      redirect to('/hello/World')
-    end
+%w{controllers models routes}.each {|dir| Dir.glob("./#{dir}/*.rb", &method(:require))}
 
-    get %r{/hello/([\w]+)} do |c|
-      "Hello, #{c}!"
-    end
+# второй вариант
+#%w{controllers models routes}.each do |dir|
+#  $LOAD_PATH << File.expand_path('', File.join(File.dirname(__FILE__).dir))
+#  require File.join(dir, 'init')
+#end
+
+get '/' do
+  'Hello My Sinatra - Easy and Wide World!'
+end
+
+get '/hello/name' do
+  "Sinatra приветствует тебя, #{params[:name]}!"
+end
+
+namespace '/api/v1' do
+  get '/*' do
+    "I don't know what is the #{params[:splat]}. It's what you typed."
   end
 end
+
+get '/v' do
+  redirect to('/hello/World')
+end
+
+get %r{/hello/([\w]+)} do |c|
+  "Hello, #{c}!"
+end
+
+get '/say/*/to/*' do
+  params[:splat].to_s
+end
+
+get '/jobs.?:format?' do
+  # соответствует "GET /jobs", "GET /jobs.json", "GET /jobs.xml" и т.д.
+  'Rout is working'
+end
+#   end
+# end
 
 
--- config.ru
+++ config.ru
@@ -5,8 +5,11 @@
 root = File.expand_path File.dirname(__FILE__)
 require File.join( root , 'app_sinatra.rb' )
 
-app = Rack::Builder.app do
-  run AppModule::App
-end
+# app = Rack::Builder.app do
+#   run AppModule::App
+# end
+#
+# run app
 
-run app
+run Sinatra::Application
+
